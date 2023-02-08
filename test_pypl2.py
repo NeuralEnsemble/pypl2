1654b877b18f7a49becbd22bc5367eef17ceda8d
Julia Sprenger <julia.sprenger@rwth-aachen.de>
2023-02-08 12:20:00 +0100

    add more comparison tests

--- test_pypl2.py
+++ test_pypl2.py
@@ -5,7 +5,7 @@ import difflib
 import pathlib
 
 from pypl2api import pl2_ad, pl2_spikes, pl2_events, pl2_info
-
+from pypl2lib import PyPL2FileReader, PL2FileInfo, PL2SpikeChannelInfo, PL2DigitalChannelInfo, PL2AnalogChannelInfo
 
 def dump_loaded_example_data(output_filename):
     """
@@ -71,10 +71,50 @@ def test_compare_loading_wenv_zugbruecke():
         os.remove(f)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        # run comparison test if no argument is provided
-        test_compare_loading_wenv_zugbruecke()
-    else:
-        output_filename = sys.argv[1]
-        dump_loaded_example_data(output_filename)
+def test_compare_FileReader_methods():
+    # Get file infos
+    filename = pathlib.Path(__file__).parent / 'data' / '4chDemoPL2.pl2'
+    reader = PyPL2FileReader()
+    reader.pl2_open_file(filename)
+
+    file_info = PL2FileInfo()
+    reader.pl2_get_file_info(file_info)
+
+    print('iuae')
+
+
+    for i in range(file_info.m_TotalNumberOfSpikeChannels):
+        # loading channel info via index, name and source methods
+        schannel_info_by_index = PL2SpikeChannelInfo()
+        schannel_info_by_name = PL2SpikeChannelInfo()
+        schannel_info_by_source = PL2SpikeChannelInfo()
+
+        reader.pl2_get_spike_channel_info(i, schannel_info_by_index)
+        channel_name = schannel_info_by_index.m_Name
+        source_id = schannel_info_by_index.m_Source
+
+        reader.pl2_get_spike_channel_info_by_name(channel_name, schannel_info_by_name)
+        reader.pl2_get_spike_channel_info_by_source(source_id, i+1, schannel_info_by_source)
+
+        # comparing results
+        for attr_name, attr_type in schannel_info_by_index._fields_:
+
+            value_by_index = getattr(schannel_info_by_index, attr_name)
+            value_by_name = getattr(schannel_info_by_name, attr_name)
+            value_by_source = getattr(schannel_info_by_source, attr_name)
+
+            # compare non-character array item-by-item
+            if 'c_char' not in str(attr_type) and '_Array_' in str(attr_type):
+                for idx in range(len(value_by_index)):
+                    assert value_by_index[idx] == value_by_name[idx] == value_by_source[idx]
+                continue
+
+            assert value_by_index == value_by_name == value_by_source
+
+
+    # TODO: Also compare other methods:
+    # reader.pl2_get_spike_channel_data()
+    # reader.pl2_get_analog_channel_info()
+    # reader.pl2_get_analog_channel_data()
+    # reader.pl2_get_digital_channel_info()
+    # reader.pl2_get_digital_channel_data()
